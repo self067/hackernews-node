c345ace0d17058c701b6541cb52e0565456d88a0
self067 <seltor@mail.ru>
2021-01-28 12:14:53 +0300

    feat: add schema & db connection

--- src/index.js
+++ src/index.js
@@ -1,23 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+const { PrismaClient } = require('@prisma/client')
 const { ApolloServer } = require('apollo-server');
 
-// 1
-const typeDefs = `
-  type Query {
-    info: String!
-  }
-`;
 
-// 2
+// 1
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+
+let idCount = links.length
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
-  }
-};
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Mutation: {
+    // 2
+    post: (parent, args) => {
+       const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    }
+  },
+}
 
-// 3
+
+const prisma = new PrismaClient()
+
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(
+    path.join(__dirname, 'schema.graphql'),
+    'utf8'
+  ),
   resolvers,
+  context: {
+    prisma,
+  }
 });
 
 server
